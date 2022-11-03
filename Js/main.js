70acd2ce631b3eb1ae9300742c424fc48981330b
Federico Galache <106155636+fico444@users.noreply.github.com>
2022-11-03 20:20:09 -0300

    javascript final

--- Js/main.js
+++ Js/main.js
@@ -1,15 +1,38 @@
 const ul = document.querySelector("ul");
-console.log(ul);
+var preSelection = 1;
 
-function selection(item){
+function seleccion(item){
+    //si se hace click sobre la seleccion activa sale
+    if(preSelection==item){
+        return;
+    }
+
+  //obtiene las referencias a los elementos de la seleccion anterior  
+    const anteriorLi = ul.children[preSelection];
+    const preH2 = anteriorLi.querySelector("h2");
+    const preArrow = anteriorLi.querySelector(".imgActive");
+    const preP = anteriorLi.querySelector("p"); 
+    
+    //obtiene las referencias a los elementos de la seleccion actual
     const selectedLi = ul.children[item];
-        
     const h2 = selectedLi.querySelector("h2");
     const arrow = selectedLi.querySelector(".arrow");
     const p = selectedLi.querySelector("p");
+   
+    // oculta la seleccion anterior
+    preH2.classList.remove("active");
+    preArrow.classList.add("arrow");
+    preArrow.classList.remove("imgActive");    
+    preP.classList.remove("active");
+    preP.classList.add("hidden");
 
-    console.log(item);
-    console.log(h2);
-    console.log(p);
-
+    //muestra la nueva seleccion
+    h2.classList.add("active");
+    arrow.classList.add("imgActive");
+    arrow.classList.remove("arrow");
+    p.classList.add("active");
+    p.classList.remove("hidden");
+    
+    //actualiza el valor de la seleccion acterior con el actual
+    preSelection = item;
 }
